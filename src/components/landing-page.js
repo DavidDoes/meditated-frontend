b5ec7a42e5c14056a6cfc7216047c1d76c74e895
David Clark <dlc.clark@gmail.com>
2019-02-04 21:18:55 -0800

    landing-page working

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 
 import LoginForm from './login-form';
 
-export function LandingPage(props) {
-    // If we are logged in redirect straight to the user's dashboard
-    if (props.loggedIn) {
-        return <Redirect to="/dashboard" />;
-    }
+import '../styles/landing-page.css';
 
-    return (
-        <div className="home">
-            <h2>Welcome to Foo App</h2>
-            <LoginForm />
-            <Link to="/register">Register</Link>
+const SignupButton = () => {
+  return (
+    <Link to="/signup">
+      <button>Signup</button>
+    </Link>
+  );
+};
+
+export function LandingPage(props) {
+  // If we are logged in redirect straight to the user's dashboard
+  if (props.loggedIn) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <div>
+      <section id="tagline">
+        <h1>simplify your meditation</h1>
+        <p>simple, distraction-free meditation tracking</p>
+        <i class="fas fa-angle-down" />
+      </section>
+
+      <section id="about">
+        <div>
+          <p>
+            You don't need expensive subscriptions to meditate.
+            <br /> You can do it right now.
+          </p>
+          <p>The secret to a strong healthy habit is consistency.</p>
+          <p>Use Meditated to keep track of your meditation habit.</p>
+          <SignupButton />
         </div>
-    );
+      </section>
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
-    loggedIn: state.auth.currentUser !== null
+  loggedIn: state.auth.currentUser !== null
 });
 
 export default connect(mapStateToProps)(LandingPage);
