6dbd889e7babf8ffd0c032b1e3aaec88a8de26a0
David Clark <dlc.clark@gmail.com>
2019-02-05 16:39:19 -0800

    scroll listener

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
+import { animateScroll, Link as animatedLink } from 'react-scroll';
 
 import '../styles/landing-page.css';
 
@@ -18,20 +19,25 @@ export function LandingPage(props) {
     return <Redirect to="/dashboard" />;
   }
 
+  const scrollDown = () => {
+    document.getElementById('about').scrollIntoView({
+      behavior: 'smooth'
+    });
+  };
+
   return (
     <div>
       <section id="tagline">
         <h1>simplify your meditation</h1>
         <p>simple, distraction-free meditation tracking</p>
-        <i class="fas fa-angle-down" />
+        <i onClick={scrollDown} class="fas fa-angle-down" />
+        {/* <animatedLink target={'#about'} smooth={true} duration={6000}>
+          <i class="fas fa-angle-down" />
+        </animatedLink> */}
       </section>
 
       <section id="about">
         <div>
-          <p>
-            You don't need expensive subscriptions to meditate.
-            <br /> You can do it right now.
-          </p>
           <p>The secret to a strong healthy habit is consistency.</p>
           <p>Use Meditated to keep track of your meditation habit.</p>
 
