b08187cb718cafdf1f717d960ca349de7cdbaa03
David Clark <dlc.clark@gmail.com>
2019-03-06 17:21:51 -0800

    move api base url to config

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080/api';
+// export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080/api';
+export const API_BASE_URL = 'https://meditated-api.herokuapp.com/api';
