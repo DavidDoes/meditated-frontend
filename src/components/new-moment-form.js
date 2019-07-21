7f31ec9ec080513257dd70288e2ba00663ef963f
David Clark <dlc.clark@gmail.com>
2019-07-21 14:03:01 -0700

    before checkout

--- src/components/new-moment-form.js
+++ src/components/new-moment-form.js
@@ -3,11 +3,7 @@ import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import '../styles/new-moment-form.css';
 import { newMoment } from '../actions/moments';
-<<<<<<< HEAD
 import LocationSelect from './location-select';
-=======
-import MomentsMenu from './moments-menu';
->>>>>>> parent of e11908a... added select to form; pulls from moments list
 import DatePicker from 'react-date-picker';
 
 export class NewMoment extends React.Component {
@@ -124,7 +120,7 @@ export class NewMoment extends React.Component {
             </div>
 
             <div className="location">
-              <MomentsMenu setLocation={this.setLocation} location={this.state.location} />
+              <LocationSelect setLocation={this.setLocation} location={this.state.location} />
             </div>
 
             <div className="date">
