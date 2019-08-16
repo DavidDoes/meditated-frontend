1c699863c401ffbd6bf512f5ca1b293096376d5a
David Clark <dlc.clark@gmail.com>
2019-08-15 18:50:04 -0700

    loading message

--- src/components/login-page.js
+++ src/components/login-page.js
@@ -6,6 +6,17 @@ import LoginForm from './login-form';
 import '../styles/login-page.css';
 
 export function LoginPage(props) {
+  const loadMsg = () => {
+    console.log(props.loading);
+    if (props.loading) {
+      return (
+        <div className="load-msg">
+          <h3>Loading...</h3>
+        </div>
+      );
+    }
+  };
+
   // If we are logged in (which happens automatically when registration
   // is successful) redirect to the user's dashboard
   if (props.loggedIn) {
@@ -14,13 +25,15 @@ export function LoginPage(props) {
   return (
     <section id="form-section">
       <h2 className="page-title">Login</h2>
+      {loadMsg()}
       <LoginForm />
     </section>
   );
 }
 
 const mapStateToProps = state => ({
-  loggedIn: state.auth.currentUser !== null
+  loggedIn: state.auth.currentUser !== null,
+  loading: state.auth.loading
 });
 
 export default connect(mapStateToProps)(LoginPage);
