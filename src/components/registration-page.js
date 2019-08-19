af2a424bcf9880d2512f4d7186625907d903145b
David Clark <dlc.clark@gmail.com>
2019-08-18 17:08:21 -0700

    add load msg to registration page

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -6,6 +6,16 @@ import RegistrationForm from './registration-form';
 import '../styles/login-page.css';
 
 export function RegistrationPage(props) {
+  const loadMsg = () => {
+    if (props.loading) {
+      return (
+        <div className="load-msg">
+          <h3>Loading...</h3>
+        </div>
+      );
+    }
+  };
+
   // If we are logged in (which happens automatically when registration
   // is successful) redirect to the user's dashboard
   if (props.loggedIn) {
@@ -14,13 +24,15 @@ export function RegistrationPage(props) {
   return (
     <section id="form-section">
       <h2 className="page-title">Signup</h2>
+      {loadMsg()}
       <RegistrationForm />
     </section>
   );
 }
 
 const mapStateToProps = state => ({
-  loggedIn: state.auth.currentUser !== null
+  loggedIn: state.auth.currentUser !== null,
+  loading: state.auth.loading
 });
 
 export default connect(mapStateToProps)(RegistrationPage);
