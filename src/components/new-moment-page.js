7c40bab51ab3ca33dac621864299326757af941a
David Clark <dlc.clark@gmail.com>
2019-08-18 17:12:43 -0700

    add load msg for new moment

--- src/components/new-moment-page.js
+++ src/components/new-moment-page.js
@@ -4,16 +4,28 @@ import { connect } from 'react-redux';
 import NewMomentForm from './new-moment-form';
 
 export function NewMomentPage(props) {
+  const loadMsg = () => {
+    if (props.loading) {
+      return (
+        <div className="load-msg">
+          <h3>Loading...</h3>
+        </div>
+      );
+    }
+  };
+
   return (
     <section id="form-section">
       <h2 className="page-title">New Moment</h2>
+      {loadMsg()}
       <NewMomentForm />
     </section>
   );
 }
 
 const mapStateToProps = state => ({
-  loggedIn: state.auth.currentUser !== null
+  loggedIn: state.auth.currentUser !== null,
+  loading: state.auth.loading
 });
 
 export default connect(mapStateToProps)(NewMomentPage);
